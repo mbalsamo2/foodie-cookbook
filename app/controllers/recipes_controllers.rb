bdb99ecbd9820e5515f8650e5641360c384d5fd2
mbalsamo <github email address>
2018-02-23 14:30:09 -0500

    creates new recipe form

--- app/controllers/recipes_controllers.rb
+++ app/controllers/recipes_controllers.rb
@@ -1,11 +1,15 @@
 class RecipesController < ApplicationController
 
-    use Rack::Flash
+  use Rack::Flash
 
   get '/recipes' do
     @user = User.find_by_id(session[:user_id])
     @recipes = Recipe.all
     erb :"/recipes/recipes"
+  end
+
+  get '/recipes/new' do
+    erb :"/recipes/create_recipe"
   end
 
 end
